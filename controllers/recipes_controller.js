a5c2efa19f47d17688251a7dceb736af91fb8acc
Lili Cohen <lili.cohen.tv@gmail.com>
2021-01-30 14:41:17 -0800

    hiding and changing key

--- controllers/recipes_controller.js
+++ controllers/recipes_controller.js
@@ -1,19 +1,22 @@
 const express = require("express");
+const dotenv = require('dotenv').config()
 
-function allRecipes(req,res) {
-   const queryURL =
-    "https://api.edamam.com/search?q=all" +
-    process.env.REACT_APP_GOOGLE_API_KEY;
+module.exports = {
+  allRecipes: function (req, res) {
+    const queryURL =
+      "https://api.edamam.com/search?q=all&app_id=" +
+      process.env.API_ID + "&app_key=" + process.env.API_KEY;
 
-   fetch({
-    url: queryURL,
-    method: "GET",
-  }).then(function (response) {
-    console.log(response)
-    res.render('recipes/recipes', {
-      layout: 'main-recipes',
+    fetch({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response)
+      res.render('recipes/recipes', {
+        layout: 'main-recipes',
+      });
     });
-  });
-};
+  }
 
-module.exports = allRecipes;
+};
+// module.exports = { allRecipes };
