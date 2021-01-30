1e0a681f76f765a7487e12f8e8fe75df34712420
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-01-30 15:48:26 -0800

    deleted user file because not part of project

--- routes/html/index.js
+++ routes/html/index.js
@@ -3,15 +3,15 @@ const db = require("../../models");
 
 
 router.get("/", function(req, res) {
-    res.render("index")
-})
+    res.render("index");
+});
 
 router.get("/facilityform", function(req, res) {
-    res.render("facilityform")
-})
+    res.render("facilityform");
+});
 
 router.get("/facility", function(req, res) {
-    res.render("facility")
-})
+    res.render("facility");
+});
 
 module.exports = router;
