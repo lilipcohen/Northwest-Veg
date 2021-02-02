999ebddb8ec93a4cd88eb6037460a5b342e050d2
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-02-02 12:37:34 -0800

    Resolving submit button

--- public/assets/js/facility.js
+++ public/assets/js/facility.js
@@ -1,35 +1,33 @@
 // once form is submitted, a new form displays on the page
-function submitForm(form) {
-    console.log("hi");
-    $.post("/facilityform", form, function () {
-        window.location.href = "/facilityform";
-    });
-}
+// function submitForm(form) {
+//     $.post("/facilityform", form, function () {
+//         window.location.href = "/facilityform";
+//     });
+// }
 
 // information of new facility from form gets displayed on facility page
-// originally the submitbt had a . instead of the #. Switched on 'submit' to on 'click'
-$("#submitbtn").on("click", function () {
-    console.log("Hello")
-    let newFacility = {
-        name: $("#facility_name").val().trim(),
-        address: $("#address").val().trim(),
-        city: $("#city").val().trim(),
-        state: $("#state").val().trim(),
-        postalcode: $("#postalcode").val().trim(),
-        email: $("#email").val().trim(),
-        description: $("#description").val().trim()
-    };
 
-   
-// passed in the information that we were receiving from the user in newFacility and passed it into submitForm
-    submitForm(newFacility);
+$("#submitbtn").on("submit", function () {
+console.log("is this working");
+    // let newFacility = {
+    //     name: $("#facility_name").val().trim(),
+    //     address: $("#address").val().trim(),
+    //     city: $("#city").val().trim(),
+    //     state: $("#state").val().trim(),
+    //     postalcode: $("#postalcode").val().trim(),
+    //     email: $("#email").val().trim(),
+    //     description: $("#description").val().trim()
+    // };
 
-    $.ajax("/api/facility", {
-        type: "POST",
-        data: newFacility
-    }).then(function () {
-        console.log("Created new facility");
-        // Reload page to get updated list
-        location.reload();
-    });
+    // submitForm(newFacility);
+
+    // $.ajax("/api/facility", {
+    //     type: "POST",
+    //     data: newFacility
+    // }).then(function () {
+    //     console.log("Created new facility");
+    //     // Reload page to get updated list
+    //     location.reload();
+    // });
 });
+
