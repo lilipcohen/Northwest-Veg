58fec251d7b5baed9b5d6687fbf998b7a0f53205
Lili Cohen <lili.cohen.tv@gmail.com>
2021-02-04 12:49:45 -0800

    changing query call to get all recipes and messing with handlebars

--- controllers/recipes_controller.js
+++ controllers/recipes_controller.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv').config()
 module.exports = {
   allRecipes: async function (req, res) {
     const queryURL =
-      "https://api.edamam.com/search?q=salad&app_id=" +
+      "https://api.edamam.com/search?q=&app_id=" +
       process.env.API_ID + "&app_key=" + process.env.API_KEY + "&from=0&to=10&health=vegetarian&health=vegan";
 
     const response = await fetch(queryURL)
