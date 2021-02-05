0cffe1da48332c32b19b5586c5ed956f23177708
Lili Cohen <lili.cohen.tv@gmail.com>
2021-02-04 22:04:14 -0800

    testing

--- routes/api/recipes.js
+++ routes/api/recipes.js
@@ -2,5 +2,5 @@ const router = require('express').Router();
 var recipes_controller = require('../../controllers/recipes_controller');
 
 router.route("/allrecipes")
-    .get(recipes_controller.allAllergen)
+    .get(recipes_controller.allRecipes)
 module.exports = router; 
