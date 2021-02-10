dda5970b1a6bbb1e522e614cd19451cd2abe1681
Lili Cohen <lili.cohen.tv@gmail.com>
2021-02-09 17:01:08 -0800

    resolving the pop up url for recipe api in handlebars

--- controllers/recipes_controller.js
+++ controllers/recipes_controller.js
@@ -5,7 +5,7 @@ module.exports = {
   allRecipes: async function (req, res) {
     const queryURL =
       "https://api.edamam.com/search?q=&app_id=" +
-      process.env.API_ID + "&app_key=" + process.env.API_KEY + "&from=0&to=10&health=vegetarian";
+      process.env.API_ID + "&app_key=" + process.env.API_KEY + "&from=0&to=10&health=vegan";
 
     const response = await fetch(queryURL)
     const result = await response.json();
@@ -18,7 +18,7 @@ module.exports = {
     allBreakfast: async function (req, res) {
     const queryURL =
       "https://api.edamam.com/search?q=&app_id=" +
-      process.env.API_ID + "&app_key=" + process.env.API_KEY + "&from=0&to=10&health=vegetarian&mealType=breakfast";
+      process.env.API_ID + "&app_key=" + process.env.API_KEY + "&from=0&to=10&health=vegan&mealType=breakfast";
 
     const response = await fetch(queryURL)
     const result = await response.json();
