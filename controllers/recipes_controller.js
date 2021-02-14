43c275340a4d087638f1cf42a1c051d7596e1ab0
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-02-14 10:16:20 -0800

    method that allows users to search for a recipe. returns back JSON data

--- controllers/recipes_controller.js
+++ controllers/recipes_controller.js
@@ -1,18 +1,20 @@
 const fetch = require("node-fetch");
 const dotenv = require('dotenv').config()
 
- async function  fetchRecipes(categoryquery) {
-      const queryURL =
-      "https://api.edamam.com/search?q=&app_id=" +
-      process.env.API_ID + "&app_key=" + process.env.API_KEY + "&from=0&to=10&health=vegan" + categoryquery;
+async function fetchRecipes(categoryquery) {
+  const queryURL =
+    "https://api.edamam.com/search?q=&app_id=" +
+    process.env.API_ID + "&app_key=" + process.env.API_KEY + "&from=0&to=10&health=vegan" + categoryquery;
+  console.log(queryURL);
+  const response = await fetch(queryURL)
 
-    const response = await fetch(queryURL)
-    const result = await response.json();
-    const sendRecipes = {
-      result: result.hits
-    }
-    return sendRecipes;
-    }
+  const result = await response.json();
+  console.log(result);
+  const sendRecipes = {
+    result: result.hits
+  }
+  return sendRecipes;
+}
 
 module.exports = {
   allRecipes: async function (req, res) {
@@ -25,32 +27,51 @@ module.exports = {
     const sendRecipes = {
       result: result.hits
     }
-    
+
     res.render("recipelist", sendRecipes);
   },
-    allBreakfast: async function (req, res) {
-    
+
+  // users search for recipe
+  searchRecipe: async function (req, res) {
+
+    const keyword = req.params.keyword
+    const queryURL =
+      "https://api.edamam.com/search?q=" + keyword + "&app_id=" +
+      process.env.API_ID + "&app_key=" + process.env.API_KEY + "&from=0&to=10&health=vegan";
+
+    const response = await fetch(queryURL)
+    const result = await response.json();
+    const sendRecipes = {
+      result: result.hits
+    }
+    res.json(result.hits)
+  },
+  
+  allBreakfast: async function (req, res) {
+
     res.render("recipelist", await fetchRecipes("&mealType=Breakfast"));
   },
-    allLunchDin: async function (req, res) {
-    
+  allLunchDin: async function (req, res) {
+
     res.render("recipelist", await fetchRecipes("&mealType=Lunch&mealType=Dinner"));
   },
-    allDesserts: async function (req, res) {
-    
+  allDesserts: async function (req, res) {
+
     res.render("recipelist", await fetchRecipes("&dishType=Desserts"));
   },
-    allSnacks: async function (req, res) {
-    
+  allSnacks: async function (req, res) {
+
     res.render("recipelist", await fetchRecipes("&mealType=Snack"));
   },
-    allGluten: async function (req, res) {
-    
+  allGluten: async function (req, res) {
+
     res.render("recipelist", await fetchRecipes("&health=gluten-free"));
   },
-    allAllergen: async function (req, res) {
-      
+  allAllergen: async function (req, res) {
+
     res.render("recipelist", await fetchRecipes("&health=peanut-free&health=tree-nut-free"));
   }
+
+
 
 };
