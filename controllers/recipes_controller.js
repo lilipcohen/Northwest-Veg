fe81f0125b850e8ebefd8d32c13cc71aebb671fe
Tomara Petty <tomararuth@gmail.com>
2021-02-17 09:22:22 -0800

    delete comments

--- controllers/recipes_controller.js
+++ controllers/recipes_controller.js
@@ -14,19 +14,6 @@ const dotenv = require('dotenv').config()
     return sendRecipes;
     }
 
-    // SEARCH BAR
-    // async function  fetchSearch(search, categoryquery) {
-    //   const queryURL =
-    //   `https://api.edamam.com/search?q=${search}&app_id=${process.env.API_ID}&app_key=${process.env.API_KEY}&from=0&to=20&health=vegan&alcohol-free${categoryquery}`;
-
-    // const response = await fetch(queryURL)
-    // const result = await response.json();
-    // const sendRecipes = {
-    //   result: result.hits
-    // }
-    // return sendRecipes;
-    // }
-
 module.exports = {
   allRecipes: async function (req, res) {
     const queryURL =
