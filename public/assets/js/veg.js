e124a90b79ce37e89469c17dff206253e0a8e142
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-02-19 21:40:18 -0800

    user search populates recipes

--- public/assets/js/veg.js
+++ public/assets/js/veg.js
@@ -19,33 +19,66 @@
 
 
 
-async function searchRecipe() {
+function searchRecipe() {
     const searchBar = document.getElementById("search");
 
     searchBar.addEventListener("keyup", e => {
 
         const userInput = e.target.value;
 
+
+
         if (e.key === "Enter") {
             console.log(this.value);
             $.ajax({
                 method: "GET",
                 url: "/api/recipe/" + userInput
+            }).then(results => {
+                console.log(results)
+       $("#main").empty();
+                results.forEach(function (response) {
+                    $("#main").append(` 
+                    
+                    <div class="recipe">
+                    <img class="home-image"
+                        src="${response.recipe.image}"
+                        alt="fruits and veggies" />
+                    <div class="recipe-info">
+                        <h3>Breakfast</h3>
+                        <span class="veggie">VEG</span>
+                    </div>
+                    <div class="overview">
+                        <h3>${response.recipe.label}</h3>
+        
+                        <br><a href="/api/recipe/allbreakfast" class="waves-effect #7cb342 light-green darken-1 btn">Get
+                            recipes</a>
+                    </div>
+                </div>
+        
+                
+                `)
 
-            }).then(function (results) {
-                //    const userSearch = JSON.parse(results);
-                   $("#main").html(results);
-                });
+
+
+                })
+
+            })
+
+
+
+
+
+
         }
 
-
     })
-
 }
 
-
-
-
-
 searchRecipe();
+
+
+
+
+
+
 
