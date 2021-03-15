32bbecfed3a98a0a05c78c936cb68e976e3c9a4c
Tomara Petty <tomararuth@gmail.com>
2021-03-15 16:58:27 -0700

    update all recipe ajax calls page rendering

--- controllers/recipes_controller.js
+++ controllers/recipes_controller.js
@@ -1,78 +1,182 @@
-const fetch = require("node-fetch");
-const dotenv = require('dotenv').config()
+const fetch = require('node-fetch');
+const dotenv = require('dotenv').config();
 
 async function fetchRecipes(categoryquery) {
   const queryURL =
-    "https://api.edamam.com/search?q=&app_id=" +
-    process.env.API_ID + "&app_key=" + process.env.API_KEY + "&from=0&to=10&health=vegan" + categoryquery;
+    'https://api.edamam.com/search?q=&app_id=' +
+    process.env.API_ID +
+    '&app_key=' +
+    process.env.API_KEY +
+    '&from=0&to=10&health=vegan' +
+    categoryquery;
   console.log(queryURL);
-  const response = await fetch(queryURL)
+  const response = await fetch(queryURL);
 
   const result = await response.json();
   console.log(result);
   const sendRecipes = {
-    result: result.hits
-  }
+    result: result.hits,
+  };
   return sendRecipes;
 }
 
 module.exports = {
   allRecipes: async function (req, res) {
     const queryURL =
-      "https://api.edamam.com/search?q=&app_id=" +
-      process.env.API_ID + "&app_key=" + process.env.API_KEY + "&from=0&to=10&health=vegan";
+      'https://api.edamam.com/search?q=&app_id=' +
+      process.env.API_ID +
+      '&app_key=' +
+      process.env.API_KEY +
+      '&from=0&to=10&health=vegan';
 
-    const response = await fetch(queryURL)
+    const response = await fetch(queryURL);
     const result = await response.json();
     const sendRecipes = {
-      result: result.hits
-    }
+      result: result.hits,
+    };
 
-    res.render("recipelist", sendRecipes);
+    res.render('recipelist', sendRecipes);
   },
 
   // users search for recipe
   searchRecipe: async function (req, res) {
-
-    const keyword = req.params.keyword
+    const keyword = req.params.keyword;
     const queryURL =
-      "https://api.edamam.com/search?q=" + keyword + "&app_id=" +
-      process.env.API_ID + "&app_key=" + process.env.API_KEY + "&from=0&to=10&health=vegan";
+      'https://api.edamam.com/search?q=' +
+      keyword +
+      '&app_id=' +
+      process.env.API_ID +
+      '&app_key=' +
+      process.env.API_KEY +
+      '&from=0&to=10&health=vegan';
 
-    const response = await fetch(queryURL)
+    const response = await fetch(queryURL);
     const result = await response.json();
     const sendRecipes = {
-      result: result.hits
-    }
+      result: result.hits,
+    };
     res.json(result.hits);
   },
-  
-  
-  allBreakfast: async function (req, res) {
 
-    res.render("recipelist", await fetchRecipes("&mealType=Breakfast"));
+  allBreakfast: async function (req, res) {
+    const queryURL =
+      'https://api.edamam.com/search?q=&app_id=' +
+      process.env.API_ID +
+      '&app_key=' +
+      process.env.API_KEY +
+      '&from=0&to=10&health=vegan&mealType=Breakfast&health=alcohol-free';
+
+    console.log(queryURL);
+    const response = await fetch(queryURL);
+
+    const result = await response.json();
+    console.log(result);
+    const sendRecipes = {
+      result: result.hits,
+    };
+    res.json(result.hits);
+    // return sendRecipes;
+    // res.render("recipelist", await fetchRecipes("&mealType=Breakfast"));
   },
   allLunchDin: async function (req, res) {
+    const queryURL =
+      'https://api.edamam.com/search?q=&app_id=' +
+      process.env.API_ID +
+      '&app_key=' +
+      process.env.API_KEY +
+      '&from=0&to=10&health=vegan&mealType=Lunch&mealType=Dinner&health=alcohol-free';
 
-    res.render("recipelist", await fetchRecipes("&mealType=Lunch&mealType=Dinner"));
+    console.log(queryURL);
+    const response = await fetch(queryURL);
+
+    const result = await response.json();
+    console.log(result);
+    const sendRecipes = {
+      result: result.hits,
+    };
+    res.json(result.hits);
+    // res.render(
+    //   'recipelist',
+    //   await fetchRecipes('&mealType=Lunch&mealType=Dinner')
+    // );
   },
   allDesserts: async function (req, res) {
+    const queryURL =
+      'https://api.edamam.com/search?q=&app_id=' +
+      process.env.API_ID +
+      '&app_key=' +
+      process.env.API_KEY +
+      '&from=0&to=10&health=vegan&dishType=Desserts&health=alcohol-free';
 
-    res.render("recipelist", await fetchRecipes("&dishType=Desserts"));
+    console.log(queryURL);
+    const response = await fetch(queryURL);
+
+    const result = await response.json();
+    console.log(result);
+    const sendRecipes = {
+      result: result.hits,
+    };
+    res.json(result.hits);
+    // res.render('recipelist', await fetchRecipes('&dishType=Desserts'));
   },
   allSnacks: async function (req, res) {
+    const queryURL =
+      'https://api.edamam.com/search?q=&app_id=' +
+      process.env.API_ID +
+      '&app_key=' +
+      process.env.API_KEY +
+      '&from=0&to=10&health=vegan&mealType=Snack&health=alcohol-free';
 
-    res.render("recipelist", await fetchRecipes("&mealType=Snack"));
+    console.log(queryURL);
+    const response = await fetch(queryURL);
+
+    const result = await response.json();
+    console.log(result);
+    const sendRecipes = {
+      result: result.hits,
+    };
+    res.json(result.hits);
+    // res.render('recipelist', await fetchRecipes('&mealType=Snack'));
   },
   allGluten: async function (req, res) {
+    const queryURL =
+      'https://api.edamam.com/search?q=&app_id=' +
+      process.env.API_ID +
+      '&app_key=' +
+      process.env.API_KEY +
+      '&from=0&to=10&health=vegan&health=gluten-free&health=alcohol-free';
 
-    res.render("recipelist", await fetchRecipes("&health=gluten-free"));
+    console.log(queryURL);
+    const response = await fetch(queryURL);
+
+    const result = await response.json();
+    console.log(result);
+    const sendRecipes = {
+      result: result.hits,
+    };
+    res.json(result.hits);
+    // res.render('recipelist', await fetchRecipes('&health=gluten-free'));
   },
   allAllergen: async function (req, res) {
+    const queryURL =
+      'https://api.edamam.com/search?q=&app_id=' +
+      process.env.API_ID +
+      '&app_key=' +
+      process.env.API_KEY +
+      '&from=0&to=10&health=vegan&health=peanut-free&health=tree-nut-free&health=gluten-free&health=alcohol-free';
 
-    res.render("recipelist", await fetchRecipes("&health=peanut-free&health=tree-nut-free"));
-  }
+    console.log(queryURL);
+    const response = await fetch(queryURL);
 
-
-
+    const result = await response.json();
+    console.log(result);
+    const sendRecipes = {
+      result: result.hits,
+    };
+    res.json(result.hits);
+    // res.render(
+    //   'recipelist',
+    //   await fetchRecipes('&health=peanut-free&health=tree-nut-free')
+    // );
+  },
 };
--- public/assets/js/veg.js
+++ public/assets/js/veg.js
@@ -1,44 +1,62 @@
-const { allRecipes } = require("../../../controllers/recipes_controller");
+$(document).ready(function () {
+  function getBreakfast() {
+    // $.ajax({
+    //   method: "GET",
+    //   url: "/api/recipe/allrecipes",
+    //   data: allBreakfast
+    // })
+    //   .then(function() {
+    // window.location.href = "/api/recipe/allbreakfast";
+    //   });
+
+    $.ajax({
+      method: 'GET',
+      url: '/api/recipe/allbreakfast',
+    }).then(results => {
+      console.log(results);
+
+      $('.userSearchRecipe').empty();
+      results.forEach(function (response) {
+        $('.userSearchRecipe').append(` 
+                
+                <div class="recipe">
+                <img class="home-image"
+                    src="${response.recipe.image}"
+                    alt="recipe" />
+                <div class="recipe-info">
+                    <h3>${response.recipe.label}</h3>
+                    <span class="veggie">VEG</span>
+                </div>
+                <div class="overview">
+                    <h3>${response.recipe.label}</h3>
+    
+                    
+                </div>
+            </div>
+    
+            
+            `);
+      });
+    });
+  }
+
+  $('.breakfast').on('click', function (event) {
+    console.log('breakfast button clicked');
+    getBreakfast();
+  });
+});
 
 $(document).ready(function () {
-    function getBreakfast(allBreakfast) {
-        // $.ajax({
-        //   method: "GET",
-        //   url: "/api/recipe/allrecipes",
-        //   data: allBreakfast
-        // })
-        //   .then(function() {
-        window.location.href = "/api/recipe/allbreakfast";
-        //   });
-    }
-    $(".breakfast").on("click", function (event) {
-        getBreakfast();
+  function getLunchDin() {
+    $.ajax({
+      method: 'GET',
+      url: '/api/recipe/alllundin',
+    }).then(results => {
+      console.log(results);
 
-    });
-
-
-
-
-function searchRecipe() {
-    const searchBar = document.getElementById("search");
-
-    searchBar.addEventListener("keyup", e => {
-
-        const userInput = e.target.value;
-        
-
-
-
-        if (e.key === "Enter") {
-            
-            $.ajax({
-                method: "GET",
-                url: "/api/recipe/" + userInput
-            }).then(results => {
-                console.log(results)
-                $(".userSearchRecipe").empty();
-                results.forEach(function (response) {
-                    $(".userSearchRecipe").append(` 
+      $('.userSearchRecipe').empty();
+      results.forEach(function (response) {
+        $('.userSearchRecipe').append(` 
                     
                     <div class="recipe">
                     <img class="home-image"
@@ -55,30 +73,204 @@ function searchRecipe() {
                     </div>
                 </div>
         
-                
-                `)
+                `);
+      });
+    });
+  }
 
+  $('.lunchdin').on('click', function (event) {
+    console.log('lunchdin button clicked');
+    getLunchDin();
+  });
+});
 
+$(document).ready(function () {
+  function getDesserts() {
+    $.ajax({
+      method: 'GET',
+      url: '/api/recipe/alldesserts',
+    }).then(results => {
+      console.log(results);
 
-                })
+      $('.userSearchRecipe').empty();
+      results.forEach(function (response) {
+        $('.userSearchRecipe').append(` 
+                        
+                        <div class="recipe">
+                        <img class="home-image"
+                            src="${response.recipe.image}"
+                            alt="recipe" />
+                        <div class="recipe-info">
+                            <h3>${response.recipe.label}</h3>
+                            <span class="veggie">VEG</span>
+                        </div>
+                        <div class="overview">
+                            <h3>${response.recipe.label}</h3>
+            
+                            
+                        </div>
+                    </div>
+            
+                    `);
+      });
+    });
+  }
 
-            })
+  $('.desserts').on('click', function (event) {
+    console.log('desserts button clicked');
+    getDesserts();
+  });
+});
 
+$(document).ready(function () {
+    function getSnacks() {
+      $.ajax({
+        method: 'GET',
+        url: '/api/recipe/allsnacks',
+      }).then(results => {
+        console.log(results);
+  
+        $('.userSearchRecipe').empty();
+        results.forEach(function (response) {
+          $('.userSearchRecipe').append(` 
+                          
+                          <div class="recipe">
+                          <img class="home-image"
+                              src="${response.recipe.image}"
+                              alt="recipe" />
+                          <div class="recipe-info">
+                              <h3>${response.recipe.label}</h3>
+                              <span class="veggie">VEG</span>
+                          </div>
+                          <div class="overview">
+                              <h3>${response.recipe.label}</h3>
+              
+                              
+                          </div>
+                      </div>
+              
+                      `);
+        });
+      });
+    }
+  
+    $('.snacks').on('click', function (event) {
+      console.log('snacks button clicked');
+      getSnacks();
+    });
+  });
 
+$(document).ready(function () {
+  function getGlutenFree() {
+    $.ajax({
+      method: 'GET',
+      url: '/api/recipe/allgluten',
+    }).then(results => {
+      console.log(results);
 
+      $('.userSearchRecipe').empty();
+      results.forEach(function (response) {
+        $('.userSearchRecipe').append(` 
+                        
+                        <div class="recipe">
+                        <img class="home-image"
+                            src="${response.recipe.image}"
+                            alt="recipe" />
+                        <div class="recipe-info">
+                            <h3>${response.recipe.label}</h3>
+                            <span class="veggie">VEG</span>
+                        </div>
+                        <div class="overview">
+                            <h3>${response.recipe.label}</h3>
+            
+                            
+                        </div>
+                    </div>
+            
+                    `);
+      });
+    });
+  }
 
+  $('.gf').on('click', function (event) {
+    console.log('snacks button clicked');
+    getGlutenFree();
+  });
+});
 
+$(document).ready(function () {
+    function getAllergen() {
+      $.ajax({
+        method: 'GET',
+        url: '/api/recipe/allallergen',
+      }).then(results => {
+        console.log(results);
+  
+        $('.userSearchRecipe').empty();
+        results.forEach(function (response) {
+          $('.userSearchRecipe').append(` 
+                          
+                          <div class="recipe">
+                          <img class="home-image"
+                              src="${response.recipe.image}"
+                              alt="recipe" />
+                          <div class="recipe-info">
+                              <h3>${response.recipe.label}</h3>
+                              <span class="veggie">VEG</span>
+                          </div>
+                          <div class="overview">
+                              <h3>${response.recipe.label}</h3>
+              
+                              
+                          </div>
+                      </div>
+              
+                      `);
+        });
+      });
+    }
+  
+    $('.allergen').on('click', function (event) {
+      console.log('allergen button clicked');
+      getAllergen();
+    });
+  });
 
-        }
+function searchRecipe() {
+  const searchBar = document.getElementById('search');
 
-    })
+  searchBar.addEventListener('keyup', e => {
+    const userInput = e.target.value;
+
+    if (e.key === 'Enter') {
+      $.ajax({
+        method: 'GET',
+        url: '/api/recipe/' + userInput,
+      }).then(results => {
+        console.log(results);
+        $('.userSearchRecipe').empty();
+        results.forEach(function (response) {
+          $('.userSearchRecipe').append(` 
+                    
+                    <div class="recipe">
+                    <img class="home-image"
+                        src="${response.recipe.image}"
+                        alt="recipe" />
+                    <div class="recipe-info">
+                        <h3>${response.recipe.label}</h3>
+                        <span class="veggie">VEG</span>
+                    </div>
+                    <div class="overview">
+                        <h3>${response.recipe.label}</h3>
+        
+                        
+                    </div>
+                </div>                
+                `);
+        });
+      });
+    }
+  });
 }
 
 searchRecipe();
-
-
-
-
-
-
-
